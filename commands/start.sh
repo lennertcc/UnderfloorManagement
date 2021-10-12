13eb53046639b332f0fedc9ad7d6279eb0f15cb6
Lennert Collée <lennert.collee@rapidcircle.com>
2021-10-12 10:57:20 +0200

    Remove nohup.out

--- commands/start.sh
+++ commands/start.sh
@@ -1,3 +1,3 @@
-nohup sudo java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:8000 -jar /home/pi/UnderfloorManagement/UnderfloorManagement-1.2-SNAPSHOT.jar &
+nohup sudo java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:8000 -jar /home/pi/UnderfloorManagement/UnderfloorManagement-1.2-SNAPSHOT.jar >/dev/null 2>&1 &
 echo $!
 echo $! > save_pid.txt
